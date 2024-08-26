110ebb68ecfcf0263fb8ab376fee83c8f15c6700
Sathwik <hsathwik002@gmail.com>
2024-08-26 20:57:06 +0530

    feath:producer_message/cli

--- producer.js
+++ producer.js
@@ -1,24 +1,36 @@
 const { kafka } = require("./client");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 async function init() {
   const producer = kafka.producer();
-  console.log("Connecting producer...");
+
+  console.log("Connecting Producer");
   await producer.connect();
-  console.log("Connection successful");
+  console.log("Producer Connected Successfully");
 
-  await producer.send({
-    topic: "riderUpdate",
-    messages: [
-      {
-        partition: 0,
-        key: "Location-message",
-        value: JSON.stringify({ name: "piyush", location: "SOUTH" }),
-      },
-    ],
+  rl.setPrompt("> ");
+  rl.prompt();
+
+  rl.on("line", async function (line) {
+    const [riderName, location] = line.split(" ");
+    await producer.send({
+      topic: "rider-updates",
+      messages: [
+        {
+          partition: location.toLowerCase() === "north" ? 0 : 1,
+          key: "location-update",
+          value: JSON.stringify({ name: riderName, location }),
+        },
+      ],
+    });
+  }).on("close", async () => {
+    await producer.disconnect();
   });
-
-  await producer.disconnect();
-  console.log("Producer disconnected");
 }
 
-init().catch(console.error);
+init();
